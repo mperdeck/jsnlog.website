6bf4b8e055cca507f434d0da22cd0c43db5c4690
mperdeck <mperdeck@gmail.com>
2015-10-09 00:34:25 +1100

    Added links to demo sites in more prominent places

--- WebSite/App_Code/SiteConstants.cs
+++ WebSite/App_Code/SiteConstants.cs
@@ -15,6 +15,8 @@ namespace WebSite.App_Code
         public const string HandlerExtension = ".logger";
         public const string DefaultDefaultAjaxUrl = "/jsnlog" + HandlerExtension;
 
+        public const string DemoGithubUrl = "https://github.com/mperdeck/jsnlogSimpleWorkingDemos";
+        public const string AngularJsDemoGithubUrl = "https://github.com/mperdeck/JSNLog.AngularJS";
         public const string LicenceUrl = "https://github.com/mperdeck/jsnlog/blob/master/LICENSE";
         public const string LicenceName = "Apache License, Version 2.0";
 
