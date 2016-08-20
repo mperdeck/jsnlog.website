c827b823537063f5d321f2154af2d51440a1f61c
Matt Perdeck <mperdeck@gmail.com>
2016-08-20 10:45:37 +1000

    2.19.2

--- WebSite/App_Code/Generated.cs
+++ WebSite/App_Code/Generated.cs
@@ -1,11 +1,11 @@
 // Generated.cs gets copied from Generated.cs.template
-// During the copy, 2.19.1 is replaced by the actual version.
+// During the copy, 2.19.2 is replaced by the actual version.
 
 namespace WebSite.App_Code
 {
     public static class Generated
     {
-        public static string Version { get { return "2.19.1";  } }
+        public static string Version { get { return "2.19.2";  } }
     }
 }
 
