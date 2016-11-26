257e1453775a5baaa58f1c898e94b14cf7b2c05c
Matt Perdeck <mperdeck@gmail.com>
2016-11-26 20:40:02 +1100

    re init

--- WebSite/App_Start/RouteConfig.cs
+++ WebSite/App_Start/RouteConfig.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace WebSite
+namespace Website
 {
     public class RouteConfig
     {
@@ -15,30 +15,8 @@ namespace WebSite
 
             routes.MapRoute(
                 name: "Default",
-                url: "",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-                namespaces: new String[] { "MainSite.Controllers" } 
-            );
-
-            routes.MapRoute(
-                name: "Default2",
-                url: "Index",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-                namespaces: new String[] { "MainSite.Controllers" } 
-            );
-
-            routes.MapRoute(
-                name: "PhpJs",
-                url: "phpjs",
-                defaults: new { controller = "Home", action = "PhpJs", id = UrlParameter.Optional },
-                namespaces: new String[] { "MainSite.Controllers" }
-            );
-
-            routes.MapRoute(
-                name: "Documentation",
-                url: "Documentation/{*pathInfo}",
-                defaults: new { controller = "Documentation", action = "Index" },
-                namespaces: new String[] { "MainSite.Controllers" } 
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
--- WebSite/Global.asax.cs
+++ WebSite/Global.asax.cs
@@ -2,26 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Http;
 using System.Web.Mvc;
-using System.Web.Optimization;
 using System.Web.Routing;
 
-namespace WebSite
+namespace Website
 {
-    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
-    // visit http://go.microsoft.com/?LinkId=9394801
-
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
             AreaRegistration.RegisterAllAreas();
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            ViewExtensionsConfig.RegisterViews();
-            ViewExtensionsConfig.RegisterPageVersions();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
         }
     }
 }
